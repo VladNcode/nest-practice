c01062c417b1d3476c97214ba456aa0de2669869
Vladyslav Nikiforov <vladncode@gmail.com>
2022-04-09 11:34:43 +0300

    added mongo connection and env files

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,10 +1,15 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { FindProductDto } from 'src/product/dto/find-product.dto';
 import { TopPageModel } from './top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly configService: ConfigService) {}
 	@Post('create')
-	async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+	async create(@Body() dto: Omit<TopPageModel, '_id'>) {
+		this.configService.get('MONGO_PORT');
+	}
 
 	@Get(':id')
 	async get(@Param('id') id: string) {}
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TopPageController } from './top-page.controller';
 
 @Module({
-  controllers: [TopPageController]
+	controllers: [TopPageController],
+	imports: [ConfigModule],
 })
 export class TopPageModule {}
