4846a0bef1683aaf4dee9471c4f123506bbd80cd
Vladyslav Nikiforov <vladncode@gmail.com>
2022-04-15 08:45:58 +0300

    file uploads

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
+import { join } from 'path';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 
 @Module({
+	imports: [
+		ServeStaticModule.forRoot({
+			// rootPath: join(__dirname, '..', 'uploads'),
+			rootPath: join(path, 'uploads'),
+			exclude: ['/api*'],
+		}),
+	],
 	controllers: [FilesController],
 	providers: [FilesService],
 })
